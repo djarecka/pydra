ee99df9c2750bc4cbc0cdb3cb4451ab6c4c80fcb
Dorota Jarecka <djarecka@gmail.com>
2019-11-21 12:20:25 -0500

    adding more tests using*

--- ci/none.sh
+++ ci/none.sh
@@ -47,7 +47,7 @@ function travis_before_script {
 
 function travis_script {
     if [ "$CHECK_TYPE" = "test" ]; then
-        pytest -vs -n auto pydra/engine/tests/test_singularity.py
+        pytest -vs -n auto pydra/engine/tests/test_s*.py
     elif [ "$CHECK_TYPE" = "style" ]; then
         black --check pydra tools setup.py
     fi
