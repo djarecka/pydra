da69712c309b79a40214620d0e2d2512dc9c1f53
Dorota Jarecka <djarecka@gmail.com>
2019-11-22 11:18:14 -0500

    adding all tests to the singularity build, removing one travis run

--- ci/singularity.sh
+++ ci/singularity.sh
@@ -28,7 +28,7 @@ function travis_before_script {
 }
 
 function travis_script {
-    pytest -vs --cov pydra --cov-config .coveragerc --cov-report xml:cov.xml --doctest-modules pydra/engine/tests/test_singularity.py
+    pytest -vs --cov pydra --cov-config .coveragerc --cov-report xml:cov.xml --doctest-modules pydra
 }
 
 function travis_after_script {
